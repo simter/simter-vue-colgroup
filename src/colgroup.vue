ff5a8695190e0aebd6840db04abfe647c09329ee
RJ.Hwang <rongjihuang@gmail.com>
2018-02-24 16:58:25 +0800

    Simplify columns config and add flatten helper function

--- src/colgroup.vue
+++ src/colgroup.vue
@@ -1,25 +1,34 @@
 <template>
   <colgroup>
-    <col v-for="w in flattenColumns" :style="{width:w}">
+    <col v-for="(width, index) in columns" :style="{width: width}" :key="index">
   </colgroup>
 </template>
 
 <script>
-  function flatten(arr) {
-    return arr.reduce((a, b) => {
-      return a.concat(b.children ? flatten(b.children) : b.width || b)
-    }, []).map(a => a.width || a)
-  }
+/**
+ * Flatten complex object array to simple string array.
+ * 
+ * Just deal with object's 'width' and 'children' key.
+ * 
+ * Example :
+ * 1. [{width: '20px'}, {width: '40px'}, ...] flatten to ['20px', '40px', ...]
+ * 2. [{width: '20px'}, {children: [{width: '40px'}, {width: '60px'}]}, ...] 
+ *    flatten to ['20px', '40px', '60px', ...]
+ */
+function flatten(columns) {
+  return columns
+    .reduce((a, b) => {
+      return a.concat(b.children ? flatten(b.children) : b.width || b);
+    }, [])
+    .map(a => a.width || a);
+}
 
-  export default {
-    replace: true,
-    props: {
-      columns: { type: Array, required: true }
-    },
-    computed: {
-      flattenColumns: function(){
-        return flatten(this.columns)
-      }
-    }
-  };
+export default {
+  replace: true,
+  props: {
+    // The column's 'width' config array, like ['20px', '40px', ...]
+    columns: { type: Array, required: true }
+  },
+  flatten: flatten // helper function
+};
 </script>
