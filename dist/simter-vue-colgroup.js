fbb6bb4278c67abecac722bcd56876f1274e2dbd
RJ.Hwang <rongjihuang@gmail.com>
2018-02-24 17:02:00 +0800

    Release version 0.2.0

--- dist/simter-vue-colgroup.js
+++ dist/simter-vue-colgroup.js
@@ -4,22 +4,31 @@
 	(global['simter-vue-colgroup'] = factory());
 }(this, (function () { 'use strict';
 
-function flatten(arr) {
-  return arr.reduce((a, b) => {
-    return a.concat(b.children ? flatten(b.children) : b.width || b)
-  }, []).map(a => a.width || a)
+/**
+ * Flatten complex object array to simple string array.
+ * 
+ * Just deal with object's 'width' and 'children' key.
+ * 
+ * Example :
+ * 1. [{width: '20px'}, {width: '40px'}, ...] flatten to ['20px', '40px', ...]
+ * 2. [{width: '20px'}, {children: [{width: '40px'}, {width: '60px'}]}, ...] 
+ *    flatten to ['20px', '40px', '60px', ...]
+ */
+function flatten(columns) {
+  return columns
+    .reduce((a, b) => {
+      return a.concat(b.children ? flatten(b.children) : b.width || b);
+    }, [])
+    .map(a => a.width || a);
 }
 
-var colgroup = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('colgroup',_vm._l((_vm.flattenColumns),function(w){return _c('col',{style:({width:w})})}))},staticRenderFns: [],
+var colgroup = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('colgroup',_vm._l((_vm.columns),function(width,index){return _c('col',{key:index,style:({width: width})})}))},staticRenderFns: [],
   replace: true,
   props: {
+    // The column's 'width' config array, like ['20px', '40px', ...]
     columns: { type: Array, required: true }
   },
-  computed: {
-    flattenColumns: function(){
-      return flatten(this.columns)
-    }
-  }
+  flatten: flatten // helper function
 };
 
 return colgroup;
